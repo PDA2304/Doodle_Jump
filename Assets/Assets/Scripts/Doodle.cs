85a4824764bd7e5f92676988a1558fe4bb4b2f22
isip_d.a.pahomov@mpt.ru <isip_d.a.pahomov@mpt.ru>
2021-01-25 02:53:22 +0300

    doodle_jump

---
+++ Assets/Assets/Scripts/Doodle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Doodle : MonoBehaviour
+{
+    public static Doodle instance;
+    float horizontal;
+    public Rigidbody2D DoodleRigid;
+    public Text text;
+    private float score = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            horizontal = Input.acceleration.x;
+        }
+
+        if (Input.acceleration.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (Input.acceleration.x > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        DoodleRigid.velocity = new Vector2(Input.acceleration.x * 10f, DoodleRigid.velocity.y);
+
+        float moveInput = Input.GetAxis("Horizontal");
+        if (moveInput < 0)
+        { // Движ влево
+            DoodleRigid.velocity = new Vector2(-5, DoodleRigid.velocity.y);
+        }
+        else if (moveInput > 0)
+        { // Движ вправо
+            DoodleRigid.velocity = new Vector2(5, DoodleRigid.velocity.y);
+        }
+
+        if (DoodleRigid.velocity.y > 0 && transform.position.y > score)
+        {
+            score = transform.position.y;
+        }
+        text.text = "СЧЁТ " + Mathf.Round(score).ToString();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "DeadZone")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/LeftRich.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRich : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if(collision.name == "Doodle")
+        {
+            if(name  == "A")
+            {
+                collision.gameObject.transform.position = new Vector3(2.1F, collision.gameObject.transform.position.y, 0f);
+
+            }
+            if (name == "B")
+            {
+                collision.gameObject.transform.position = new Vector3(-2.1F, collision.gameObject.transform.position.y, 0f);
+            }
+        }
+    }
+}
